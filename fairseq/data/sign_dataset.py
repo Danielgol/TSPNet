77259d467802b01ca3e52983476e402f4b964457
Daniel Rosa <dan.rosa10@gmail.com>
2022-05-05 13:59:49 -0300

    testando phoenix

--- fairseq/data/sign_dataset.py
+++ fairseq/data/sign_dataset.py
@@ -18,7 +18,11 @@ class SignDataset(FairseqDataset):
         identifier = self.dataset[index]
         all_features = []
         for level_feat_roots in self.feat_roots:
+
             print(level_feat_roots, os.path.join(identifier + '.pt'))
+            tensor = torch.load(os.path.join(level_feat_roots[0], identifier + '.pt'))
+            print(level_feat_roots, os.path.join(identifier + '.pt'), tensor.shape)
+
 
 
             features = [torch.cat(torch.load(os.path.join(feat_root, identifier + '.pt')), dim=0)
@@ -26,8 +30,8 @@ class SignDataset(FairseqDataset):
             features = torch.cat(features, dim=1)
             all_features.append(features)
 
-        for i in range(len(all_features)):
-            print(all_features[i].shape)
+        #for i in range(len(all_features)):
+        #    print(all_features[i].shape)
         #print(len(all_features))
         #print("\n\n\nOPAAAAAAAAAAAAA:",len(all_features),"\n\n\n\n")
         #return all_features
