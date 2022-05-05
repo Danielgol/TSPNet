039557410ddf042512af63abaeacf9c0156bb96d
Daniel Rosa <dan.rosa10@gmail.com>
2022-05-05 11:21:15 -0300

    testando phoenix

--- test_scripts/test_sign_local.py
+++ test_scripts/test_sign_local.py
@@ -141,7 +141,7 @@ def validate(args, trainer, task, epoch_itr, subsets):
         with metrics.aggregate(new_root=True) as agg:
             hyps, refs = [], []
 
-            print(len(progress))
+            print("\n\n\n\n",len(progress),"\n\n\n\n")
 
             for sample in progress:
                 logging_output, h, r = trainer.valid_step(sample, generate=True)
