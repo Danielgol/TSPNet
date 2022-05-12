a34a1f87b27476cd24de22c42a961a8f6d4d0661
Daniel Rosa <dan.rosa10@gmail.com>
2022-05-12 12:13:49 -0300

    teste target

--- fairseq/tasks/translation_sign.py
+++ fairseq/tasks/translation_sign.py
@@ -46,9 +46,6 @@ def load_langpair_dataset(
 
     print(len(src_dataset), len(tgt_dataset))
 
-    for src in src_dataset:
-        print(len(src[0]))
-
     logger.info('{} {} {}-{} {} examples'.format(
         data_path, split, src, tgt, len(src_dataset)
     ))
