8b7eb7d5cb0a43ff1b06705eaac61b7cf57cda49
Daniel Rosa <dan.rosa10@gmail.com>
2022-05-16 20:13:01 -0300

    testando nomes

--- fairseq/tasks/translation_sign.py
+++ fairseq/tasks/translation_sign.py
@@ -213,7 +213,7 @@ class TranslationSign(FairseqTask):
             multilv_args=multilv_args
         )
 
-        print(slp_dataset.src.identifiers)
+        print(slp_dataset.src.dataset)
 
         self.datasets[split] = slp_dataset
 
